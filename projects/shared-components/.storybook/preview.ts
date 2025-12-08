4fd4650bd89acdf6b4d189c0ac23104cdbc30158
Felix Heinrich <f.heinrich@srp-gmbh.de>
2025-12-08 14:40:11 +0100

    enable mat-icons

--- projects/shared-components/.storybook/preview.ts
+++ projects/shared-components/.storybook/preview.ts
@@ -1,17 +1,31 @@
-import type { Preview } from '@storybook/angular'
+import { applicationConfig, type Preview } from "@storybook/angular";
 import { setCompodocJson } from "@storybook/addon-docs/angular";
 import docJson from "../documentation.json";
+import { MatIconRegistry } from "@angular/material/icon";
+import { provideHttpClient } from "@angular/common/http";
+import { inject, provideAppInitializer } from "@angular/core";
 setCompodocJson(docJson);
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
+  decorators: [
+    applicationConfig({
+      providers: [
+        provideHttpClient(),
+        provideAppInitializer(() => {
+          const iconRegistry = inject(MatIconRegistry);
+          iconRegistry.setDefaultFontSetClass("material-symbols-outlined");
+        }),
+      ],
+    }),
+  ],
 };
 
 export default preview;
