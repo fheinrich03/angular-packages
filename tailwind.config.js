150cc2ddeed3175bcb26b8379bda333c4c018330
Felix Heinrich <f.heinrich@srp-gmbh.de>
2025-12-08 12:35:59 +0100

    fix: tailwind config - content pattern

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./projects/**/*.{html,ts}",
-    "./stories/**/*.{html,ts,tsx}",
+    "./projects/*/src/**/*.{html,ts}",
+    "./stories/*/src/**/*.{html,ts,tsx}",
   ],
   theme: {
     extend: {},
